31bd1a93aeb20c6a5de0b80479d03fd94c8bd0be
james-thx1919 <james@beachmarshalls.ca>
2026-03-18 18:41:34 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,10 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  browserLocalPersistence,
+  setPersistence,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -12,7 +17,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+
+// Force local persistence so auth survives redirects
+setPersistence(auth, browserLocalPersistence);
